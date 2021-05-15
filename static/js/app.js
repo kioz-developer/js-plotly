066ebe058f0f33b9e4d31527d36d677e79d4ecfb
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-15 09:08:09 -0500

    Fill subjects

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,19 @@ const url_data = "./samples.json";
 
 // Fetch the JSON data with D3
 d3.json(url_data).then(data => {
-    console.log(data);
+    //console.log(data.names);
+    
+    fillSubjets(data.names);
 });
+
+function fillSubjets(names) {
+    var selDataset = d3.select("#selDataset");
+    
+    var selection = selDataset.selectAll("option")
+        .data(names);
+        
+    selection.enter()
+        .append("option")
+        .merge(selection)
+        .text(d => d);
+}
