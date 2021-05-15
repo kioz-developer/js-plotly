ba6091d4c0fbd401dfec28fc75b41cedd071a7ba
Jonathan Ruiz <kioz.developer@gmail.com>
2021-05-15 12:25:35 -0500

    Filling demographic info

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,46 @@
 const url_data = "./samples.json";
-
+let data = 
 // Fetch the JSON data with D3
-d3.json(url_data).then(data => {
-    //console.log(data.names);
-    
-    fillSubjets(data.names);
+d3.json(url_data).then(d => {
+    data = d;
+
+    fill_subjets(d.names);
+    //fill_metadata(data);
 });
 
-function fillSubjets(names) {
-    var selDataset = d3.select("#selDataset");
+function optionChanged(value) {
+    let metadata = data.metadata.filter(d => d.id == value);
+
+    let list = [];
+    for ([key, value] of Object.entries(metadata[0])) {
+        console.log(key, value);
+        list.push({'key': key, 'value': value});
+    }
+
+    fill_metadata(list);
+    console.log(list);
+}
+
+function fill_subjets(names) {
+    var dataset = d3.select("#selDataset");
     
-    var selection = selDataset.selectAll("option")
+    var selection = dataset.selectAll("option")
         .data(names);
         
     selection.enter()
         .append("option")
         .merge(selection)
         .text(d => d);
+}
+
+function fill_metadata(metadata) {
+    var dataset = d3.select("#metadata");
+
+    var selection = dataset.selectAll("p")
+        .data(metadata);
+
+    selection.enter()
+        .append("p")
+        .merge(selection)
+        .text(d => `${d.key}: ${d.value}`);
 }
